8ddb1dc30457107a1460e7ad206459ac22d02f10
asryanani1997 <asryan.ani1997@gmail.com>
2023-04-09 20:01:34 +0400

    Task 1

---
+++ Task1.py
@@ -0,0 +1,11 @@
+name=input("enter the name=>")
+
+if name == "Darth Vader":
+    print ("Luke, I am your father.")
+elif name == "Leia":
+    print ("Luke, I am your sister.")
+elif name == "Han":
+    print ("Luke, I am your brother in law.")
+elif name == "R2D2":
+    print ("Luke, I am your droid.")
+        
