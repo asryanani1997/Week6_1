abe59003d51f7a1eeba80f4a819dcbbffa47eaf1
asryanani1997 <asryan.ani1997@gmail.com>
2023-04-10 17:33:44 +0400

    Task 8

---
+++ Task8.py
@@ -0,0 +1,12 @@
+x= ("6")
+y=("3")
+z=("divide")
+
+if z=="add":
+    print(str(int(x)+int(y)))
+elif z=="subtract":
+    print(str(int(x)-int(y)))
+elif z=="divide":
+    print(str(round(int(x)/int(y))))
+elif z=="multiply":
+    print(str(int(x)*int(y)))
